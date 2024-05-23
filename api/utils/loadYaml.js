883a769f35dc63daaaa57d5927360b11f946b108
chodak166 <chodak166@op.pl>
2024-05-23 08:15:21 +0200

    Added environment variables support in yaml configuration file

--- api/utils/loadYaml.js
+++ api/utils/loadYaml.js
@@ -4,6 +4,12 @@ const yaml = require('js-yaml');
 function loadYaml(filepath) {
   try {
     let fileContents = fs.readFileSync(filepath, 'utf8');
+
+    // Replace expressions like ${env:MY_VARIABLE} with actual environment variables
+    fileContents = fileContents.replace(/\${env:([^}]+)}/g, (match, envVar) => {
+      return process.env[envVar] || match; // Replace with env var value or keep the original if not defined
+    });
+
     return yaml.load(fileContents);
   } catch (e) {
     return e;
